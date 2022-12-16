83b2a5346c6cedf0fbfbf4648660add27846774d
abdulmujeeb29 <abdulmujeebtiamiyu@gmail.com>
2022-12-16 10:41:48 +0100

    whitenoise

--- mydebtor/settings.py
+++ mydebtor/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework',
     'contend',
+    'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic', 
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'mydebtor.urls'
@@ -140,6 +144,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
